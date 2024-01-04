fa4bdca61f3458f2a4f51dd399eadfb80d0e71c2
Samuel <Samuel@192.168.0.21>
2024-01-03 18:34:00 -0800

    initiation code

--- src/a/DemoA.java
+++ src/a/DemoA.java
@@ -7,5 +7,10 @@ public class DemoA {
 		System.out.println("sam is good");
 		System.out.println("git hub is good ");
 	}
+	
+	// spring 2 automation 
+	public static void main(String[] args) {
+		System.out.println(a + b);
+	}
 
 }
